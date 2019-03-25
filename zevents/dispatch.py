13265c3a5c89c26d22be478e72acae386fa636a2
Thierry Chappuis <thierry@chappuis.io>
2019-03-25 18:34:21 +0100

    Added a mixin to define a zevent listener

--- zevents/dispatch.py
+++ zevents/dispatch.py
@@ -5,6 +5,12 @@
 
 from functools import wraps
 
+def _subscribe_marked_events(instance):
+    for key, obj in type(instance).__dict__.items():
+        if hasattr(obj, "_zevents"):
+            zevents = obj._zevents
+            for zevent in zevents:
+                zevent.subscribe(getattr(instance, key))
 
 def listener(cls):
     """Class decorator to mark a class as a zevent listener."""
@@ -14,16 +20,17 @@ def listener(cls):
     # event handlers
     @wraps(cls.__init__)
     def new_init(self, *args, **kwargs):
+        _subscribe_marked_events(self)
         func(self, *args, **kwargs)
-        # Subscribe methods in __dict__ marked as handler for a given
-        # event type
-        for key, obj in cls.__dict__.items():
-            if hasattr(obj, "_zevents"):
-                zevents = obj._zevents
-                for zevent in zevents:
-                    zevent.subscribe(getattr(self, key))
 
     # Patching the constructor
     cls.__init__ = new_init
     return cls
+
+class Listener:
+    """Mixin defining a class as a zevent listener."""
+
+    def __init__(self):
+        """Subscribes bound methods to the zevents they want to listen to."""
+        _subscribe_marked_events(self)
 
