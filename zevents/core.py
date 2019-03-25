347c22e5dfe7f21a341d5bdd08d6e3efd26d40d2
Thierry Chappuis <thierry@chappuis.io>
2019-03-25 14:29:16 +0100

    Corrected bugs

--- zevents/core.py
+++ zevents/core.py
@@ -57,7 +57,7 @@ class EventManager:
         """Processes pending subscriptions."""
         # We empty the subscription queue
         queue = getattr(self, f'_{action}')
-        while not queue.empty()
+        while not queue.empty():
             event_type, handler = queue.get()
             ref = weakref.ref
             if inspect.ismethod(handler) and hasattr(handler, '__self__'):
@@ -71,7 +71,7 @@ class EventManager:
                     action == 'unsubscriptions' and
                     weak_handler in self._subscribers[klass]
                 ):
-                del self._subscribers[klass][weak_handler]
+                    del self._subscribers[klass][weak_handler]
 
 
     def _notify(self):
@@ -84,9 +84,9 @@ class EventManager:
                 handler(event)
 
     @contextmanager
-    def _non_blocking_lock():
+    def _non_blocking_lock(self):
         """Context manager for non-blocking acquisition of self._lock."""
-        locked = self._lock.acquire(blocking=False):
+        locked = self._lock.acquire(blocking=False)
         try:
             yield locked
         finally:
@@ -94,3 +94,59 @@ class EventManager:
                 self._lock.release()
 
 
+class Event:
+    """Base class of all events in the notification system.
+
+    Event is the base class used to create all the specialized events. It
+    provides default implementations to send notification for the underlying
+    event or to subscribe/unsubscribe to/from the notification feed of this
+    particular event.
+    """
+
+    manager = EventManager()
+
+    def __init__(self):
+        """Default constructor."""
+        self.name = 'Generic Event'
+
+    @classmethod
+    def send(cls, *args, **kargs):
+        """Sends a notification to all the subscribers listening to this event.
+        """
+        event = cls(*args, **kargs)
+        cls.manager.send(event)
+
+    @classmethod
+    def subscribe(cls, handler):
+        """Subscribes a handler function to the notification feed of this event.
+        """
+        cls.manager.subscribe(cls, handler)
+
+    @classmethod
+    def unsubscribe(cls, handler):
+        """Unsubscribes a handler function from the notification feed of this
+        event.
+        """
+        cls.manager.unsubscribe(cls, handler)
+
+    @classmethod
+    def listen(cls, func):
+        params = list(inspect.signature(func).parameters.keys())
+        if len(params) == 1:
+            # This is a normal function
+            cls.subscribe(func)
+        elif len(params) == 2 and params[0] == 'self':
+            # This is an unbound instance function
+            if not hasattr(func, "_zevents"):
+                setattr(func, "_zevents", [])
+            func._zevents.append(cls)
+        else:
+            raise TypeError(
+                f"Cannot subscribe {func.__qualname__}. Incorrect signature."
+            )
+
+        return func
+
+    def __str__(self):
+        """Converts the current event to a string representation."""
+        return f'<{type(self).__name__} at {id(self)}>'
--- zevents/events.py
+++ zevents/events.py
@@ -2,66 +2,9 @@
 
 """Module defining standard events for the ZEvents system."""
 
-from .manager import EventManager
+import inspect
 
-class Event:
-    """Base class of all events in the notification system.
-
-    Event is the base class used to create all the specialized events. It
-    provides default implementations to send notification for the underlying
-    event or to subscribe/unsubscribe to/from the notification feed of this
-    particular event.
-    """
-
-    manager = EventManager()
-
-    def __init__(self):
-        """Default constructor."""
-        self.name = 'Generic Event'
-
-    @classmethod
-    def send(cls, *args, **kargs):
-        """Sends a notification to all the subscribers listening to this event.
-        """
-        event = cls(*args, **kargs)
-        cls.manager.send(event)
-
-    @classmethod
-    def subscribe(cls, handler):
-        """Subscribes a handler function to the notification feed of this event.
-        """
-        cls.manager.subscribe(cls, handler)
-
-    @classmethod
-    def unsubscribe(cls, handler):
-        """Unsubscribes a handler function from the notification feed of this
-        event.
-        """
-        cls.manager.unsubscribe(cls, handler)
-
-    @classmethod
-    def listen(cls, func):
-        params = list(inspect.signature(func).parameters.keys())
-        if len(params) == 1:
-            # This is a normal function
-            cls.subscribe(func)
-        elif len(params) == 2 and params[0] == 'self':
-            # This is an unbound instance function
-            if not hasattr(func, "_zevents"):
-                setattr(func, "_zevents", [])
-            func._zevents.append(cls)
-        else:
-            raise TypeError(
-                f"Cannot subscribe {func.__qualname__}. Incorrect signature."
-            )
-
-        return func
-
-    def __str__(self):
-        """Converts the current event to a string representation."""
-        return f'<{type(self).__name__} at {id(self)}>'
-
-
+from .core import Event
 
 class TickEvent(Event):
     """Event raised by an application-level event loop."""
